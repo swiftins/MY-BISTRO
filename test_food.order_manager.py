386f5d90944dc9d5d429382ed355fcfc9029dd8f
Ivan Vorobev <ivan.vorobjov@gmail.com>
2025-02-13 13:51:02 +0200

    Added order manager module with readme file

---
+++ test_food.order_manager.py
@@ -0,0 +1,43 @@
+from db_module import DBConnector, DBManager
+from order_manager import FoodOrderManager
+
+# Инициализация подключения к базе данных
+db_connector = DBConnector('sqlite')
+db_manager = DBManager(db_connector)
+food_order_manager = FoodOrderManager(db_manager)
+
+# Создание пользователя
+food_order_manager.create_user(123456, "john_doe", "John", "Doe")
+
+# Получение категорий меню
+categories = food_order_manager.get_menu_categories()
+print("Категории меню:", categories)
+
+# Получение блюд из категории
+menu_items = food_order_manager.get_menu_items(category_id=1)
+print("Блюда из категории 1:", menu_items)
+
+# Создание заказа
+user_id = "user_id_из_базы_данных"
+food_order_manager.create_order(user_id, total_price=100)
+
+# Добавление блюда в заказ
+order_id = "order_id_из_базы_данных"
+food_order_manager.add_item_to_order(order_id, menu_item_id=1, quantity=2)
+
+# Получение заказов пользователя
+orders = food_order_manager.get_user_orders(user_id)
+print("Заказы пользователя:", orders)
+
+# Получение блюд в заказе
+order_items = food_order_manager.get_order_items(order_id)
+print("Блюда в заказе:", order_items)
+
+# Обновление статуса заказа
+food_order_manager.update_order_status(order_id, "completed")
+
+# Удаление заказа
+food_order_manager.delete_order(order_id)
+
+# Закрытие соединения с базой данных
+db_manager.close()
